f3d453ffa9244a324fa054ca644a070d0dc99113
Олег Ларионов <m2008656@edu.misis.ru>
2023-09-14 23:18:50 +0300

    2nd task done

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,36 @@
 /**
- * sortStrings - sorts array of string by two criteria "asc" or "desc"
- * @param {string[]} arr - the array of strings
+ * sortStrings - sorts aay of string by two criteria "asc" or "desc"
+ * @param {string[]} arr - the aay of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let a = [...arr]
+    for (let i = 0; i < a.length; i++) {
+        for (let j = i; j < a.length; j++) {
+            let s1 = a[i].toLowerCase()
+            let s2 = a[j].toLowerCase()
+            if(s1 === s2) {
+                // console.log(s1, s2)
+                if(a[i][0] > a[j][0]) { 
+                  let t = a[i]
+                  a[i] = a[j]
+                  a[j] = t
+                  }
+                  continue;
+            }
+            if(a[i].localeCompare(a[j]) > 0 ) {
+                let t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+    }
+    if(param === 'asc') {
+        return a   
+    }
+    else{
+        return a.reverse()
+    } 
 
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,10 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    for (const [key, value] of Object.entries(obj)) {
+        if(fields.includes(key)){
+            delete obj[key] 
+        }
+    }
+    return obj
 };
