112dd044e55d9baac52e80992a12ab1b02f8e13b
Олег Ларионов <m2008656@edu.misis.ru>
2023-10-02 23:39:51 +0300

    5th task only destroying left

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,54 +1,70 @@
 export default class NotificationMessage {
-    constructor(message = "", options = {}) {
-        const { duration = 1000, type = "success" } = options;
-    
-        this.message = message;
-        this.duration = duration;
-        this.type = type;
-        this.element = this.createElement();
-        // this.element = null
-      }
-    createElement() {
-        const element = document.createElement('div');
-        element.innerHTML = this.getTemplate();
-        
-        return element
-    }
-    getDelayTimeInSeconds(milliseconds) {
-        return milliseconds / 1000
-    }
-    getTemplate() {
-        return `<div class="notification ${this.type}" style="--value:${this.getDelayTimeInSeconds(this.duration)}s">
-        <div class="timer"></div>
+  constructor(message = "", options = {}) {
+    const { duration = 1000, type = "success" } = options;
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement();
+    // alert(this.element)
+    // this.element = null
+    this.remove()
+  }
+  createElement() {
+    const element = document.createElement('div');
+    element.classList.add("notification", this.type);
+    element.setAttribute("style", `--value: ${this.getDelayTimeInSeconds(this.duration)}s;`);
+
+    element.innerHTML = this.getTemplate();
+
+    // const element = this.getTemplate();
+    return element
+  }
+  getDelayTimeInSeconds(milliseconds) {
+    return milliseconds / 1000
+  }
+  getTemplate() {
+    return `<div class="timer"></div>
         <div class="inner-wrapper">
           <div class="notification-header">${this.type}</div>
           <div class="notification-body">
             ${this.message}
           </div>
-        </div>
-      </div>`
-    }
-    destroy() {
-        // setTimeout( this.remove,  this.duration)
-        // this.element.remove()
-        document.querySelector('.notification').parentElement.remove()
-    }
-    remove() {
-        // this.element.remove()
-        // alert(Object.entries(this)) 
-        setTimeout( this.destroy,  this.duration)
-        // setTimeout( this.element.remove,  this.duration)
+        </div>`
+  }
+  destroy() {
+    this.element.remove()
+    // document.querySelector('.notification').parentElement.remove()
+    // setTimeout( this.remove,  this.duration)
+    // this.element.remove()
+  }
+  removeLogic() {
+    document.querySelector('.notification').remove()
 
-    }
-    show() {
-        // const el = document.getElementById('btn1');
-        // el.innerHTML = this.getTemplate();
-        // this.remove()
+  }
+  remove() {
+    setTimeout(this.removeLogic, this.duration)
+    // this.element.remove()
+    // alert(Object.entries(this)) 
+    // setTimeout( this.element.remove,  this.duration)
 
+  }
+  show(div) {
+    // const el = document.getElementById('btn1');
+    // el.innerHTML = this.getTemplate();
+    // this.remove()
 
-        const el = this.createElement()
-        this.element = el
-        document.body.appendChild(el)
-        this.remove()
+    if (div) {        // const element = document.createElement('div');
+      div.classList.add("notification", this.type);
+      div.setAttribute("style", `--value: ${this.getDelayTimeInSeconds(this.duration)}s;`);
+      div.innerHTML = this.getTemplate();
     }
+    else {
+      const el = this.createElement()
+      this.element = el
+      document.body.appendChild(el)
+    }
+    this.remove()
+    
+  }
 }
