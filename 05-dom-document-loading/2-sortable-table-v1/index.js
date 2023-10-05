3a621dfcdf936bb5569ab58d4b38e745eb148716
Oleg Larionov <Oleg.Larionov@ixcellerate.ru>
2023-10-05 18:05:08 +0300

    5-2 progress

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -4,8 +4,17 @@ export default class SortableTable {
     this.data = [...data];
 
     this.element = this.createElement()
+    console.log(this.data)
+    console.log(this.headerConfig)
+    // this.render()
   }
+  // render() {
+  //   const element = document.createElement("div");
+  //   element.classList.add("sortable-table");
+  //   this.element = element;
 
+  //   // this.update();
+  // }
   createElement() {
     const root = document.getElementById('root');
     root.innerHTML = this.getTemplate();
@@ -16,40 +25,38 @@ export default class SortableTable {
         <div class="sortable-table__cell">
           <img class="sortable-table-image" alt="Image" src="http://magazilla.ru/jpg_zoom1/246743.jpg">
         </div>
-        <div class="sortable-table__cell">3D очки Epson ELPGS03</div>
+        <div class="sortable-table__cell">${el.title}</div>
 
-        <div class="sortable-table__cell">16</div>
-        <div class="sortable-table__cell">91</div>
-        <div class="sortable-table__cell">6</div>
-      </a>`)
+        <div class="sortable-table__cell">${el.quantity}</div>
+        <div class="sortable-table__cell">${el.price}</div>
+        <div class="sortable-table__cell">${el.sales}</div>
+      </a>`).join('')
+
+  }
+  getHeaderTemplate() {
+    return this.headerConfig.map((el) =>
+      `<div class="sortable-table__cell" data-id="${el.id}" data-sortable="${el.sortable}" data-order="asc">
+        <span>${el.title}</span>
+      </div>`
+    ).join('')
   }
   getTemplate() {
-    return ` <div data-element="productsContainer" class="products-list__container">
-      <div data-element="header" class="sortable-table__header sortable-table__row">
-        <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-          <span>Image</span>
+    return `<div class="sortable-table">
+      
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+          ${this.getHeaderTemplate()}
         </div>
-        <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-          <span>Name</span>
-          <span data-element="arrow" class="sortable-table__sort-arrow">
-            <span class="sort-arrow"></span>
-          </span>
-        </div>
-        <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
-          <span>Quantity</span>
-        </div>
-        <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
-          <span>Price</span>
-        </div>
-        <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
-          <span>Sales</span>
-        </div>
-      </div>
 
-      <div data-element="body" class="sortable-table__body">
-        ${this.getTableRowsTemplate()}
-      </div>
+        <div data-element="body" class="sortable-table__body">
+          ${this.getTableRowsTemplate()}
+        </div>
     </div>`
+  }
+  destroy() {
+    this.remove()
+  }
+  remove() {
+    this.element.remove();
   }
 }
 
